e214745c63740b71396ce8878b785c893171f2ae
Daniel <usrrufus12@omint.com.br>
2025-11-01 15:33:42 -0300

    atualizado robots para o site ser achado por IA

--- app/robots.ts
+++ app/robots.ts
@@ -1,16 +1,19 @@
 // app/robots.ts
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://logosnext.com.br'
+  const baseUrl = "https://logosnext.com.br";
 
   return {
     rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
+      { userAgent: "*", allow: "/" },
+      { userAgent: "GPTBot", allow: "/" },
+      { userAgent: "Google-Extended", allow: "/" },
+      { userAgent: "CCBot", allow: "/" },
+      { userAgent: "ClaudeBot", allow: "/" },
+      { userAgent: "PerplexityBot", allow: "/" },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
-  }
+    host: baseUrl,
+  };
 }
